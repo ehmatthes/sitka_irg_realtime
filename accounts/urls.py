97b7b4b8e874eb8ae4a9ed9ffc45111525184e6d
Eric Matthes <eric@Erics-MacBook-Pro.local>
2020-12-13 22:52:32 -0900

    Profile page works, but unstyled.

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,6 +8,7 @@ app_name = 'accounts'
 urlpatterns = [
     # Override django-allauth logout url. It asks to confirm logout, this 
     #   is just a simple, direct logout and redirect to home page.
-    path('logout/', views.logout_view, name='logout_view')
+    path('logout/', views.logout_view, name='logout_view'),
+    path('profile/', views.profile, name='profile'),
 ]
 
--- accounts/views.py
+++ accounts/views.py
@@ -3,8 +3,14 @@ from django.http import HttpResponseRedirect
 from django.contrib.auth import logout
 from django.urls import reverse
 
+from .models import CustomUser
+
 
 def logout_view(request):
     """Log user out, and redirect to home page."""
     logout(request)
     return HttpResponseRedirect(reverse('irg_viz:index'))
+
+def profile(request):
+    """Simple user profile page."""
+    return render(request, 'account/profile.html')
