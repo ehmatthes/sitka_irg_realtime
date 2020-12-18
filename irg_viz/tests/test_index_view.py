ac9afa9f688ed8e7e42625eca3805164c04cf2a7
Eric Matthes <eric@Erics-MacBook-Pro.local>
2020-12-17 18:08:59 -0900

    Tests for what should be on anonymous page.

--- irg_viz/tests/test_index_view.py
+++ irg_viz/tests/test_index_view.py
@@ -13,6 +13,9 @@ def test_anonymous_available(rf):
     Test they don't see sensitive data.
     - No image.
     - No admin tools.
+
+    Note: This may fail, because response.content.decode() sometimes
+      breaks lines up in unexpected ways.
     """
 
     request = rf.get('/')
@@ -20,3 +23,13 @@ def test_anonymous_available(rf):
     response = views.index(request)
 
     assert response.status_code == 200
+
+    response_text = response.content.decode()
+
+    # Make assertions about what should be on anonymous page.
+    required_texts = [
+        "This is an experimental project",
+        "you must be logged in",
+    ]
+    for text in required_texts:
+        assert text in response_text
